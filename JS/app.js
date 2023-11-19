cddb24a6606f1de9c021da472f75ed2521a037bd
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2023-11-19 14:11:28 -0600

    Cambio20

---
+++ JS/app.js
@@ -0,0 +1,39 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_FUGnhu5nGn3pMul3RWMg9cQOPk6v7lc",
+    authDomain: "prueba-invernadero.firebaseapp.com",
+    databaseURL: "https://prueba-invernadero-default-rtdb.firebaseio.com",
+    projectId: "prueba-invernadero",
+    storageBucket: "prueba-invernadero.appspot.com",
+    messagingSenderId: "28136162632",
+    appId: "1:28136162632:web:a2693143d9e0075b731f7d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference to the database
+const database = firebase.database();
+
+// References to the database nodes
+const ledRef = database.ref('Invernadero/Actuador/Led');
+const potenciometroRef = database.ref('Invernadero/Sensores/Potenciometro');
+const variableSPRef = database.ref('Invernadero/Variable/sP');
+
+// Listen for changes in the LED state
+ledRef.on('value', (snapshot) => {
+    const estadoLed = snapshot.val();
+    document.getElementById('estadoLed').innerText = `Estado del LED: ${estadoLed}`;
+});
+
+// Listen for changes in the potentiometer value
+potenciometroRef.on('value', (snapshot) => {
+    const valorPotenciometro = snapshot.val();
+    document.getElementById('valorPotenciometro').innerText = `Valor del Potenciómetro: ${valorPotenciometro}`;
+});
+
+// Update the sP variable in the database
+function actualizarVariableSP() {
+    const nuevoValorSP = document.getElementById('nuevoValorSP').value;
+    variableSPRef.set(parseInt(nuevoValorSP, 10));
+}
